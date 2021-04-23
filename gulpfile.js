d70845a656a5992a737325b4431856d960065a70
bakalina.yaroslav@gmail.com <bakalina.yaroslav@gmail.com>
2021-04-23 10:49:05 +0300

    gulp watch add

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
 let gulp = require('gulp');
 let sass = require('gulp-sass');
 
-gulp.task('sass', function(){
-    gulp.src('./styles/*.scss').pipe(sass()).pipe(gulp.dest('./styles'))
-} )
+gulp.task('sass', function(done){
+    gulp.src('./styles/*.scss').pipe(sass()).pipe(gulp.dest('./styles')), done()
+} );
+
+gulp.task('watch', function(){
+    gulp.watch('./styles/*.scss', gulp.series('sass'))
+});
+
