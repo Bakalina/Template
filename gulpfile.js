5d665b9fda096d798b39e9698076aac9d5734151
bakalina.yaroslav@gmail.com <bakalina.yaroslav@gmail.com>
2021-04-26 11:13:20 +0300

    main-about-us-1

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ let sass = require('gulp-sass');
 
 gulp.task('sass', function(done){
     gulp.src('./styles/*.scss').pipe(sass()).pipe(gulp.dest('./styles')), done()
-} );
+});
 
 gulp.task('watch', function(){
     gulp.watch('./styles/*.scss', gulp.series('sass'))
